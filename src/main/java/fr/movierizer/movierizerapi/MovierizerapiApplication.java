ff48cc2cd197fb2f3fb809a408ddc6f1ff537924
s0nren <piard.hugo2004@gmail.com>
2025-04-28 07:19:23 +0000

    feat : add the auto reload

--- src/main/java/fr/movierizer/movierizerapi/MovierizerapiApplication.java
+++ src/main/java/fr/movierizer/movierizerapi/MovierizerapiApplication.java
@@ -2,6 +2,9 @@ package fr.movierizer.movierizerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
 public class MovierizerapiApplication {
